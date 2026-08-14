44b44adc9f4d2e0032d386f236fd402302de3f8a
Torikul-01 <tarikulislamtori288@gmail.com>
2026-08-14 14:49:35 +0600

    updated

---
+++ js_11_practice/task1.js
@@ -0,0 +1,8 @@
+let str='hello';
+let arr=['h','e','l','l','o'];
+console.log(str.length);
+console.log(arr.length);
+console.log(str[0]);
+console.log(arr[0]);
+console.log(arr.push('!'));
+console.log(arr);

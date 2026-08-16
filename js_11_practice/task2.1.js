2287f9b4e8830e7372c2ab4213e3b108ad6ec2a2
Torikul-01 <tarikulislamtori288@gmail.com>
2026-08-16 12:44:27 +0600

    updated

---
+++ js_11_practice/task2.1.js
@@ -0,0 +1,8 @@
+let name = "JavaScript";
+console.log(name.toUpperCase());
+
+console.log(name.toLowerCase());
+
+
+let hi="     hi there     ";
+console.log(hi.trim());
---
+++ js_11_practice/task2.js
@@ -0,0 +1,8 @@
+let string="Javascript";
+let array=string.split('');
+console.log(array);
+
+
+
+let array2=["c","a","t"];
+console.log(array2.join(''));
